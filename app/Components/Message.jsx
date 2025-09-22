b9c2a0c3cf6895054cf9f3b683d919fcd51ccbfe
Harsh <gamerpandeyharsh@gmail.com>
2025-09-22 22:37:33 +0530

    Meassage Section Completed

--- app/Components/Message.jsx
+++ app/Components/Message.jsx
@@ -3,21 +3,23 @@ import React from "react";
 const Message = () => {
   return (
     <div className="bg-[#7F3B2D] w-full">
-      <div className="w-3/4 bg-green-200 mx-auto">
-        <div className="msg-wrapper flex flex-col items-center justify-center font-Antonio uppercase">
-          <h1 className="first-message text-9xl text-center">Stir up your fearless past and</h1>
+      <div className="w-3/4 mx-auto">
+        <div className="msg-wrapper py-24 relative flex flex-col items-center justify-center font-Antonio uppercase font-bold text-[#FAEADE]">
+          <h1 className="first-message text-8xl text-center">Stir up your <br /> fearless past and</h1>
 
-          <div className="bg-[#E2A458] md:pb-5 pb-3 px-5">
-            <h2 className="text-[#7F3B2D] text-9xl">Fuel Up</h2>
+         <div className="bg-[#7F3B2D] p-2 rotate-3 absolute z-10 -translate-y-14">
+             <div className="bg-[#E2A458] md:pb-5 pb-3 px-5">
+            <h2 className="text-[#7F3B2D] text-8xl">Fuel Up</h2>
           </div>
+         </div>
 
-          <h1 className="second-message text-9xl">
+          <h1 className="second-message text-8xl text-center pt-18">
             your future with every gulp of Perfect Protein
           </h1>
 
           <div className="flex items-center justify-center md:mt-20 mt-10">
             <div className="max-w-md px-10 flex items-center justify-center overflow-hidden">
-              <p className="text-9xl whitespace-nowrap">
+              <p className="text-xs font-inter font-semibold text-center">
                 Rev up your rebel spirit and feed the adventure of life with
                 SPYLT, where you’re one chug away from epic nostalgia and
                 fearless fun.
